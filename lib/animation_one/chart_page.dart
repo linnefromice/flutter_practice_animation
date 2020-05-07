d8a13218cfd03dacc54866eb8d8b0d76163896f3
linnefromice <linnefromice@gmail.com>
2020-05-08 02:13:04 +0900

    create animation

--- lib/animation_one/chart_page.dart
+++ lib/animation_one/chart_page.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
+import 'dart:ui' show lerpDouble;
 
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
 class ChartPage extends StatefulWidget {
@@ -7,19 +9,45 @@ class ChartPage extends StatefulWidget {
   _State createState() => _State();
 }
 
-class _State extends State<ChartPage> {
+class _State extends State<ChartPage> with TickerProviderStateMixin {
   final random = Random();
   int dataSet;
+  AnimationController animation;
+  double startHeight;
+  double currentHeight;
+  double endHeight;
 
   @override
   void initState() {
     super.initState();
     dataSet = 50;
+    animation = AnimationController(
+      duration: Duration(milliseconds: 300),
+      vsync: this
+    )
+    ..addListener(() {
+      setState(() {
+        currentHeight = lerpDouble(startHeight, endHeight, animation.value);
+      });
+    });
+    startHeight = 0.0;
+    currentHeight = 0.0;
+    endHeight = dataSet.toDouble();
+    animation.forward();
+  }
+
+  @override
+  void dispose() {
+    animation.dispose();
+    super.dispose();
   }
 
   void changeData() {
     setState(() {
+      startHeight = currentHeight;
       dataSet = random.nextInt(100);
+      endHeight = dataSet.toDouble();
+      animation.forward(from: 0.0);
     });
   }
 
