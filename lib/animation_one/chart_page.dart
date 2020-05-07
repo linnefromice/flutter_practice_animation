ac03eccca84240d5310f4bdf1c060b5323b99756
linnefromice <linnefromice@gmail.com>
2020-05-08 01:58:28 +0900

    add chart bar (create BarChartPainter)

--- lib/animation_one/chart_page.dart
+++ lib/animation_one/chart_page.dart
@@ -11,6 +11,12 @@ class _State extends State<ChartPage> {
   final random = Random();
   int dataSet;
 
+  @override
+  void initState() {
+    super.initState();
+    dataSet = 50;
+  }
+
   void changeData() {
     setState(() {
       dataSet = random.nextInt(100);
@@ -21,7 +27,10 @@ class _State extends State<ChartPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Data set: $dataSet'),
+        child: CustomPaint(
+          size: Size(200.0, 100.0),
+          painter: BarChartPainter(dataSet.toDouble()),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.refresh),
@@ -29,4 +38,30 @@ class _State extends State<ChartPage> {
       ),
     );
   }
+}
+
+class BarChartPainter extends CustomPainter {
+  static double barWidth = 10.0;
+
+  BarChartPainter(this.barHeight);
+  final double barHeight;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint()
+        ..color = Colors.blue[400]
+        ..style = PaintingStyle.fill;
+    canvas.drawRect(
+      Rect.fromLTWH(
+        (size.width - barWidth) / 2.0,
+        size.height - barHeight,
+        barWidth,
+        barHeight
+      ),
+      paint
+    );
+  }
+
+  @override
+  bool shouldRepaint(BarChartPainter old) => barHeight != old.barHeight;
 }
