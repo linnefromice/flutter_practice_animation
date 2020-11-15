76b14d4bf4457f77b273b9bd1729613237f669e5
linnefromice <linnefromice@gmail.com>
2020-11-16 00:05:21 +0900

    modify AlignmentTransition - use flag & switch forward/back

--- lib/screens/align_transition_screen.dart
+++ lib/screens/align_transition_screen.dart
@@ -8,8 +8,7 @@ class AlignTransitionScreen extends StatefulWidget {
 class _State extends State<AlignTransitionScreen> with SingleTickerProviderStateMixin {
   AnimationController _animationController;
   bool _isTopLeft;
-  Animation<Alignment> _alignmentTopLeft;
-  Animation<Alignment> _alignmentBottomRight;
+  Animation<Alignment> _alignment;
 
   @override
   void initState() {
@@ -19,16 +18,10 @@ class _State extends State<AlignTransitionScreen> with SingleTickerProviderState
         vsync: this
     );
     _isTopLeft = true;
-    _alignmentTopLeft = _animationController.drive(
+    _alignment = _animationController.drive(
         AlignmentTween(
           begin: Alignment.topLeft,
           end: Alignment.bottomRight
-        )
-    );
-    _alignmentBottomRight = _animationController.drive(
-        AlignmentTween(
-          begin: Alignment.bottomRight,
-          end: Alignment.topLeft
         )
     );
   }
@@ -43,7 +36,7 @@ class _State extends State<AlignTransitionScreen> with SingleTickerProviderState
   Widget build(BuildContext context) {
     return Scaffold(
       body: AlignTransition(
-        alignment: _isTopLeft ? _alignmentBottomRight : _alignmentTopLeft,
+        alignment: _alignment,
         child: Container(
           width: 100,
           height: 100,
@@ -52,10 +45,8 @@ class _State extends State<AlignTransitionScreen> with SingleTickerProviderState
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _animationController.forward();
-          setState(() {
-            _isTopLeft = !_isTopLeft;
-          });
+          _isTopLeft ? _animationController.forward() : _animationController.reverse();
+          _isTopLeft = !_isTopLeft;
         },
         child: Icon(Icons.refresh),
       ),
