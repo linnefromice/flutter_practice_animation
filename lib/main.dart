94f06c6c4f8c3293cebb367fa5394c9a6c6b4e80
linnefromice <linnefromice@gmail.com>
2020-11-15 20:35:13 +0900

    use page_transition in home

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:flutter_practice_animation/screens/chart_page.dart';
 import 'package:flutter_practice_animation/screens/flick_screen.dart';
 import 'package:flutter_practice_animation/screens/gesture_detector_screen.dart';
@@ -18,14 +19,55 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomeScreen(),
-        '/chart': (context) => ChartPage(),
-        '/liquid_swipe': (context) => LiquidSwipeScreen(),
-        '/flick': (context) => FlickScreen(),
-        '/gesture_detector': (context) => GestureDetectorScreen(),
-        '/gesture_detector_two': (context) => GestureDetectorTwoScreen(),
+      home: HomeScreen(),
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/':
+            return PageTransition(
+              child: HomeScreen(),
+              type: PageTransitionType.size,
+              settings: settings
+            );
+            break;
+          case '/chart':
+            return PageTransition(
+                child: ChartPage(),
+                type: PageTransitionType.rotate,
+                settings: settings
+            );
+            break;
+          case '/liquid_swipe':
+            return PageTransition(
+                child: LiquidSwipeScreen(),
+                type: PageTransitionType.rightToLeftWithFade,
+                settings: settings
+            );
+            break;
+          case '/flick':
+            return PageTransition(
+                child: FlickScreen(),
+                type: PageTransitionType.leftToRightWithFade,
+                settings: settings
+            );
+            break;
+          case '/gesture_detector':
+            return PageTransition(
+                child: GestureDetectorScreen(),
+                type: PageTransitionType.fade,
+                settings: settings
+            );
+            break;
+          case '/gesture_detector_two':
+            return PageTransition(
+                child: GestureDetectorTwoScreen(),
+                type: PageTransitionType.fade,
+                duration: Duration(seconds: 1),
+                settings: settings
+            );
+            break;
+          default:
+            return null;
+        }
       },
     );
   }
