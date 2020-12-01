6a42e1ec168beec6ab059e7576d84e467adaabbb
linnefromice <linnefromice@gmail.com>
2020-12-01 11:58:47 +0900

    step 1,2

--- lib/screens/physics_simulation_screen.dart
+++ lib/screens/physics_simulation_screen.dart
@@ -5,10 +5,43 @@ class PhysicsSimulationScreen extends StatefulWidget {
   _State createState() => _State();
 }
 
-class _State extends State<PhysicsSimulationScreen> {
+class _State extends State<PhysicsSimulationScreen> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Alignment _dragAlignment = Alignment.center;
+
+  @override
+  void initState() {
+    _controller = AnimationController(vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    final Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: GestureDetector(
+        onPanDown: (details) {},
+        onPanUpdate: (details) {
+          setState(() {
+            _dragAlignment += Alignment(
+              details.delta.dx / (size.width / 2),
+              details.delta.dy / (size.height / 2),
+            );
+          });
+        },
+        onPanEnd: (details) {},
+        child: Align(
+          alignment: _dragAlignment,
+          child: FlutterLogo()
+        ),
+      )
+    );
   }
 }
