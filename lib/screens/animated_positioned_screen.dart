66ddbf4da4acae9ac0f8c097354ce3031700f75a
linnefromice <linnefromice@gmail.com>
2020-12-02 02:35:27 +0900

    add hearts including different animation

--- lib/screens/animated_positioned_screen.dart
+++ lib/screens/animated_positioned_screen.dart
@@ -8,21 +8,29 @@ class AnimatedPositionedScreen extends StatefulWidget {
 class _State extends State<AnimatedPositionedScreen> {
   bool isClick = false;
 
+  Widget _buildAnimatedPositionedHeart(final double fromLeft, final double toLeft) {
+    return AnimatedPositioned(
+      duration: Duration(milliseconds: 500),
+      top: isClick ? -50 : MediaQuery.of(context).size.height * 0.70 - 25,
+      left: isClick ? toLeft : fromLeft,
+      child: Icon(
+        Icons.favorite,
+        color: Colors.pink,
+        size: 50,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          AnimatedPositioned(
-            duration: Duration(milliseconds: 500),
-            top: isClick ? -10 : MediaQuery.of(context).size.height * 0.70 - 25,
-            left: MediaQuery.of(context).size.width * 0.5 - 25,
-            child: Icon(
-              Icons.favorite,
-              color: Colors.pink,
-              size: 50,
-            ),
-          )
+          _buildAnimatedPositionedHeart(MediaQuery.of(context).size.width * 0.20 - 25, MediaQuery.of(context).size.width * -0.10 - 25),
+          _buildAnimatedPositionedHeart(MediaQuery.of(context).size.width * 0.35 - 25, MediaQuery.of(context).size.width * 0.20 - 25),
+          _buildAnimatedPositionedHeart(MediaQuery.of(context).size.width * 0.50 - 25, MediaQuery.of(context).size.width * 0.50 - 25),
+          _buildAnimatedPositionedHeart(MediaQuery.of(context).size.width * 0.65 - 25, MediaQuery.of(context).size.width * 0.80 - 25),
+          _buildAnimatedPositionedHeart(MediaQuery.of(context).size.width * 0.80 - 25, MediaQuery.of(context).size.width * 1.10 - 25),
         ]
       ),
       floatingActionButton: FloatingActionButton(
