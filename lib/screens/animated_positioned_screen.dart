d61aaa91f73f663dce010c75947b5e187612db5e
linnefromice <linnefromice@gmail.com>
2020-12-02 02:28:15 +0900

    add animation - move top

--- lib/screens/animated_positioned_screen.dart
+++ lib/screens/animated_positioned_screen.dart
@@ -6,11 +6,31 @@ class AnimatedPositionedScreen extends StatefulWidget {
 }
 
 class _State extends State<AnimatedPositionedScreen> {
+  bool isClick = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("AnimatedPositionedScreen"),
+      body: Stack(
+        children: [
+          AnimatedPositioned(
+            duration: Duration(milliseconds: 500),
+            top: isClick ? -10 : MediaQuery.of(context).size.height * 0.70 - 25,
+            left: MediaQuery.of(context).size.width * 0.5 - 25,
+            child: Icon(
+              Icons.favorite,
+              color: Colors.pink,
+              size: 50,
+            ),
+          )
+        ]
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            isClick = true;
+          });
+        },
       ),
     );
   }
