2afebd9803b19692b25123f8eb4f772b6f057f00
linnefromice <linnefromice@gmail.com>
2020-12-02 02:11:30 +0900

    add screen

---
+++ lib/screens/animated_positioned_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AnimatedPositionedScreen extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<AnimatedPositionedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("AnimatedPositionedScreen"),
+      ),
+    );
+  }
+}
