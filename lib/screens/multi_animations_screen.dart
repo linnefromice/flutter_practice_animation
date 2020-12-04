c65dec78de2a4acfaefe74216abeb720fc95ed14
linnefromice <linnefromice@gmail.com>
2020-12-05 00:34:01 +0900

    add animation switch

--- lib/screens/multi_animations_screen.dart
+++ lib/screens/multi_animations_screen.dart
@@ -7,18 +7,61 @@ class MultiAnimationsScreen extends StatefulWidget {
   _State createState() => _State();
 }
 
-class _State extends State<MultiAnimationsScreen> {
+class _State extends State<MultiAnimationsScreen> with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  Tween<double> _sizeTween;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 100)
+    )..addListener(() {
+      setState(() {});
+    });
+    _sizeTween = Tween<double>(
+      begin: 50,
+      end: 100
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: WrapperCommonBackground(
         child: Center(
           child: RatedHeart(
-            size: 50,
+            size: _sizeTween.evaluate(_animationController),
             rate: 1.0,
           ),
         )
       ),
+      floatingActionButton: Column( // 本来非推奨
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: 'forward',
+            child: Icon(Icons.arrow_forward),
+            onPressed: () {
+              _animationController.forward();
+            },
+          ),
+          FloatingActionButton(
+            heroTag: 'back',
+            child: Icon(Icons.arrow_back),
+            onPressed: () {
+              _animationController.reverse();
+            },
+          ),
+        ],
+      )
     );
   }
 }
