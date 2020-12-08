c77563a1129ce87bcd442ac17eef6d11db6faa5c
linnefromice <linnefromice@gmail.com>
2020-12-09 01:00:21 +0900

    add animation - update rated hearts

--- lib/screens/animated_rating_hearts_screen.dart
+++ lib/screens/animated_rating_hearts_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class AnimatedRatingHeartsScreen extends StatefulWidget {
@@ -5,18 +7,42 @@ class AnimatedRatingHeartsScreen extends StatefulWidget {
   _State createState() => _State();
 }
 
-class _State extends State<AnimatedRatingHeartsScreen> {
-  Widget _buildAnimatedPositionedHeart(final double fromLeft, final double toLeft) {
-    return AnimatedPositioned(
-      duration: Duration(milliseconds: 500),
-      top: MediaQuery.of(context).size.height * 0.70 - 25,
-      left: fromLeft,
-      child: Icon(
-        Icons.favorite,
-        color: Colors.pink,
-        size: 50,
-      ),
-    );
+class _State extends State<AnimatedRatingHeartsScreen> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation _animation;
+  Path _firstPath;
+  Path _secondPath;
+  Path _thirdPath;
+  Path _fourthPath;
+  Path _fifthPath;
+
+  @override
+  void initState() {
+    _controller = AnimationController(vsync: this, duration: Duration(milliseconds: 1000));
+    super.initState();
+    _animation = Tween(begin: 0.0,end: 1.0).animate(_controller)
+      ..addListener((){
+        setState(() {});
+      });
+    _firstPath = drawFirstPath();
+    _secondPath = drawSecondPath();
+    _thirdPath = drawThirdPath();
+    _fourthPath = drawFourthPath();
+    _fifthPath = drawFifthPath();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Offset calculate(value, path) {
+    PathMetrics pathMetrics = path.computeMetrics();
+    PathMetric pathMetric = pathMetrics.elementAt(0);
+    value = pathMetric.length * value;
+    Tangent pos = pathMetric.getTangentForOffset(value);
+    return pos.position;
   }
 
   Path drawFirstPath() {
@@ -54,16 +80,44 @@ class _State extends State<AnimatedRatingHeartsScreen> {
     return path;
   }
 
+  Widget _buildAnimatedPositionedHeart(final double top, final double left) {
+    return AnimatedPositioned(
+      duration: Duration(milliseconds: 500),
+      top: top,
+      left: left,
+      child: Icon(
+        Icons.favorite,
+        color: Colors.pink,
+        size: 50,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          _buildAnimatedPositionedHeart(MediaQuery.of(context).size.width * 0.20 - 25, MediaQuery.of(context).size.width * -0.10 - 25),
-          _buildAnimatedPositionedHeart(MediaQuery.of(context).size.width * 0.35 - 25, MediaQuery.of(context).size.width * 0.20 - 25),
-          _buildAnimatedPositionedHeart(MediaQuery.of(context).size.width * 0.50 - 25, MediaQuery.of(context).size.width * 0.50 - 25),
-          _buildAnimatedPositionedHeart(MediaQuery.of(context).size.width * 0.65 - 25, MediaQuery.of(context).size.width * 0.80 - 25),
-          _buildAnimatedPositionedHeart(MediaQuery.of(context).size.width * 0.80 - 25, MediaQuery.of(context).size.width * 1.10 - 25),
+          _buildAnimatedPositionedHeart(
+            MediaQuery.of(context).size.height * 0.70 - 25 + calculate(_animation.value, _firstPath).dy,
+            MediaQuery.of(context).size.width * 0.20 - 25 + calculate(_animation.value, _firstPath).dx
+          ),
+          _buildAnimatedPositionedHeart(
+            MediaQuery.of(context).size.height * 0.70 - 25 + calculate(_animation.value, _secondPath).dy,
+            MediaQuery.of(context).size.width * 0.35 - 25 + calculate(_animation.value, _secondPath).dx
+          ),
+          _buildAnimatedPositionedHeart(
+            MediaQuery.of(context).size.height * 0.70 - 25 + calculate(_animation.value, _thirdPath).dy,
+            MediaQuery.of(context).size.width * 0.50 - 25 + calculate(_animation.value, _thirdPath).dx
+          ),
+          _buildAnimatedPositionedHeart(
+            MediaQuery.of(context).size.height * 0.70 - 25 + calculate(_animation.value, _fourthPath).dy,
+            MediaQuery.of(context).size.width * 0.65 - 25 + calculate(_animation.value, _fourthPath).dx
+          ),
+          _buildAnimatedPositionedHeart(
+            MediaQuery.of(context).size.height * 0.70 - 25 + calculate(_animation.value, _fifthPath).dy,
+            MediaQuery.of(context).size.width * 0.80 - 25 + calculate(_animation.value, _fifthPath).dx
+          ),
           Positioned(
             top: MediaQuery.of(context).size.height * 0.70,
             left: MediaQuery.of(context).size.width * 0.20,
@@ -103,9 +157,7 @@ class _State extends State<AnimatedRatingHeartsScreen> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          setState(() {
-            // isClick = true;
-          });
+          _controller.forward();
         },
       ),
     );
