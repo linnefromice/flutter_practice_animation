3df1b68705d258636dc96f8cb70e8e7571fdb0e4
linnefromice <linnefromice@gmail.com>
2020-12-10 02:07:27 +0900

    add rating function

--- lib/screens/animated_rating_hearts_two_screen.dart
+++ lib/screens/animated_rating_hearts_two_screen.dart
@@ -12,11 +12,13 @@ class AnimatedRatingHeartsTwoScreen extends StatefulWidget {
 }
 
 class _State extends State<AnimatedRatingHeartsTwoScreen> with SingleTickerProviderStateMixin {
+  double _sumRating;
   AnimationController _controller;
 
   @override
   void initState() {
     super.initState();
+    _sumRating = 0.0;
     _controller = AnimationController(
       vsync: this,
       duration: Duration(milliseconds: 1000)
@@ -42,24 +44,56 @@ class _State extends State<AnimatedRatingHeartsTwoScreen> with SingleTickerProvi
     );
   }
 
-  List<Widget> _createRatingHearts(double sumRating) {
+  List<Widget> _createRatingHearts({final double baseTop, final double iconSize, final double sumRating}) {
     final int count = 5;
     final leftWeightingFactors = [0.20, 0.35, 0.50, 0.65, 0.80];
-    final double iconSize = 50;
     return List.generate(count, (index) =>
       _buildPositionedHeart(
         min(1, max(0, sumRating - index)),
         iconSize,
-        MediaQuery.of(context).size.height * 0.70 - iconSize * 0.50,
+        baseTop - iconSize * 0.50,
         MediaQuery.of(context).size.width * leftWeightingFactors[index] - iconSize * 0.50,
       )
     );
   }
 
+  Widget _buildGestureDetectorWidget(final double wrappedWidgetWidth) {
+    return GestureDetector(
+      onVerticalDragEnd: (details) {
+        // TODO: start animation
+      },
+      onHorizontalDragUpdate: (details) {
+        setState(() {
+          _sumRating = details.localPosition.dx/wrappedWidgetWidth * 5;
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final double _heartIconSize = 50;
+    final double _baseTop = MediaQuery.of(context).size.height * 0.70;
+    final double _widgetsWidth = MediaQuery.of(context).size.width * 0.70;
 
-    List<Widget> _children = _createRatingHearts(3);
+    List<Widget> _children = _createRatingHearts(
+      baseTop: _baseTop,
+      iconSize: _heartIconSize,
+      sumRating: _sumRating
+    );
+    _children.add(Positioned(
+      top: _baseTop - _heartIconSize,
+      left: MediaQuery.of(context).size.width * 0.15,
+      height: 100,
+      width: _widgetsWidth * 0.70,
+      child: _buildGestureDetectorWidget(_widgetsWidth)
+    ));
+    _children.add(Positioned(
+      top: _baseTop - _heartIconSize,
+      left: MediaQuery.of(context).size.width * 0.5 - 10,
+      child: Text(_sumRating.toStringAsPrecision(2))
+    ));
+
     return Scaffold(
       body: WrapperCommonBackground(
         child: Stack(
