1f06cbbabd91f96362cb04ce21fac8daf30a8728
linnefromice <linnefromice@gmail.com>
2020-12-11 01:42:35 +0900

    delete debug code

--- lib/screens/animated_rating_hearts_two_screen.dart
+++ lib/screens/animated_rating_hearts_two_screen.dart
@@ -45,27 +45,6 @@ class _State extends State<AnimatedRatingHeartsTwoScreen> with SingleTickerProvi
         to: Duration(milliseconds: 3000),
         curve: Curves.easeInOutSine,
         tag: 'heart_beat'
-      )
-      .addAnimatable(
-        animatable: Tween<double>(begin: 0.0, end: 1.0),
-        from: Duration(milliseconds: 0),
-        to: Duration(milliseconds: 1000),
-        curve: Curves.easeInOut,
-        tag: 'first_number'
-      )
-      .addAnimatable(
-        animatable: ColorTween(begin: Colors.black, end: Colors.blueAccent),
-        from: Duration(milliseconds: 2000),
-        to: Duration(milliseconds: 3000),
-        curve: Curves.easeInOut,
-        tag: 'color'
-      )
-      .addAnimatable(
-        animatable: Tween<double>(begin: 4.0, end: 5.0),
-        from: Duration(milliseconds: 4000),
-        to: Duration(milliseconds: 5000),
-        curve: Curves.easeInOut,
-        tag: 'second_number'
       ).animate(_controller);
   }
 
@@ -216,26 +195,6 @@ class _State extends State<AnimatedRatingHeartsTwoScreen> with SingleTickerProvi
       top: _baseTop - _heartIconSize,
       left: MediaQuery.of(context).size.width * 0.5 - 10,
       child: Text(_sumRating.toStringAsPrecision(2))
-    ));
-    _children.add(Positioned( // TODO: debug code for flutter_sequence_animation
-      top: _baseTop + 150,
-      left: MediaQuery.of(context).size.width * 0.5 - 35,
-      child: Text(
-        "${_sequenceAnimation['first_number'].value} -> ${_sequenceAnimation['second_number'].value}",
-        style: TextStyle(
-          color: _sequenceAnimation['color'].value
-        ),
-      ),
-    ));
-    _children.add(Positioned( // TODO: debug code for flutter_sequence_animation
-        top: _baseTop + 175,
-        left: MediaQuery.of(context).size.width * 0.5 - 45,
-        child: RaisedButton(
-          color: Colors.red,
-          onPressed: () {
-            _playAnimation();
-          },
-        )
     ));
     return _children;
   }
