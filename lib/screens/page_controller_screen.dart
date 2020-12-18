83752cd0bc5968e874ba5fa5d6dfea2aeecdc504
linnefromice <linnefromice@gmail.com>
2020-12-18 20:36:19 +0900

    create new screen

---
+++ lib/screens/page_controller_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_practice_animation/components/wrapper_common_background.dart';
+
+class PageControllerScreen extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<PageControllerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return WrapperCommonBackground();
+  }
+}
